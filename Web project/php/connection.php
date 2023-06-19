c2acef41d343e96349a0b78950352e1b6bdd1f27
justin <justinlitcom777@gmail.com>
2023-06-19 11:20:24 +0200

    update connections work for both ports

--- Web project/php/connection.php
+++ Web project/php/connection.php
@@ -1,14 +1,23 @@
 <?php
-$servername = "Localhost";
+$host = "localhost";
+$ports = array(3306, 3308);
 $username = "root";
 $password = "";
 $dbname = "groenlinks";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = null;
+foreach ($ports as $port) {
+    $conn = new mysqli($host . ":" . $port, $username, $password, $dbname);
+    if (!$conn->connect_error) {
+        break;
+    }
+}
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+echo "Connected successfully to port " . $conn->host_info;
 
 $conn->close();
 ?>
