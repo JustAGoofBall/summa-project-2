67e112df67bb8c6a97c424bfb62184853e87444c
justin <justinlitcom777@gmail.com>
2023-06-20 10:14:28 +0200

    update alle pictures in de mappen

--- Web project/leden.php
+++ Web project/leden.php
@@ -24,31 +24,78 @@
             </ul>
         </nav>
     </header>
-    <main> 
+    <main>
         <h1>ONZE LEDEN KUN JE HIER BEKIJKEN</h1>
+        <h2>Eerste kamer</h2>
+        <div class="leden-container">
+            <div class="card">
+                <img src="ledenfoto's/Tweedekamer/Jesse_Klaver_portret.jpg" alt="">
+                <p>Jesse Klaver</p>
+            </div>
+
+            <div class="card">
+                <img src="ledenfoto's/Tweedekamer/Corinne Ellemeet_ portret.jpg" alt="">
+                <p>Corinne Ellemeet</p>
+            </div>
+
+            <div class="card">
+                <img src="ledenfoto's/Tweedekamer/Kauthar_Lotte_Dale (10).jpg" alt="">
+                <p>Kauthar Bouchallikht</p>
+            </div>
+
+            <div class="card">
+                <img src="ledenfoto's/Tweedekamer/Laura Bromet_4.jpg" alt="">
+                <p>Laura Bromet</p>
+            </div>
+
+            <div class="card">
+                <img src="ledenfoto's/Tweedekamer/Lisa Westerveld portret.jpg" alt="">
+                <p>Lisa Westerveld</p>
+            </div>
+
+            <div class="card">
+                <img src="ledenfoto's/Tweedekamer/Senna Maatoug_Portret 1a.jpg" alt="">
+                <p>Senna Maatoug</p>
+            </div>
+
+            <div class="card">
+                <img src="ledenfoto's/Tweedekamer/Suzanne Kröger_portret.jpg" alt="">
+                <p>Suzanne Kröger</p>
+            </div>
+
+            <div class="card">
+                <img src="ledenfoto's/Tweedekamer/Tom van der Lee_4.jpg" alt="">
+                <p>Tom van der Lee</p>
+            </div>
+        </div>
+
+
+
+        <h3>Tweede kamer</h3>
+        <h4>Europees parlement</h4>
     </main>
     <footer>
         <div>
-        <ul>
-            <li><a href="home.php">Home</a></li>
-            <li><a href="Word_lid.php">Word Lid</a></li>
-            <li><a href="doneer.php">Doneer</a></li>
-        </ul>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="Word_lid.php">Word Lid</a></li>
+                <li><a href="doneer.php">Doneer</a></li>
+            </ul>
         </div>
 
         <div>
-        <ul>
-            <li><a href="leden.php">Onze Leden</a></li>
-            <li><a href="standpunten.php">Standpunten</a></li>
-            <li><a href="contact_us.php">Contact Us</a></li>
-        </ul>
+            <ul>
+                <li><a href="leden.php">Onze Leden</a></li>
+                <li><a href="standpunten.php">Standpunten</a></li>
+                <li><a href="contact_us.php">Contact Us</a></li>
+            </ul>
         </div>
 
         <div>
-        <ul>
-            <li><a href="">Adres</a></li>
-            <li><a href="">Telefoonummer</a></li>
-        </ul>
+            <ul>
+                <li><a href="">Adres</a></li>
+                <li><a href="">Telefoonummer</a></li>
+            </ul>
         </div>
 
         <div>
