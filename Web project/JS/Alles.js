736a37d6271d1788681e40a111cfc52ebb13bbcb
Kimohc <chomikjakub@gmail.com>
2023-06-20 20:47:43 +0200

    popups zijn af

--- Web project/JS/Alles.js
+++ Web project/JS/Alles.js
@@ -2,3 +2,17 @@ function togglepopup()
 {
     document.getElementById("popup1").classList.toggle("active");
 }
+
+function togglepopup2()
+{
+    document.getElementById("popup2").classList.toggle("active1");
+}
+function togglepopup3()
+{
+    document.getElementById("popup3").classList.toggle("active2");
+}
+
+function togglepopup4()
+{
+    document.getElementById("popup4").classList.toggle("active3");
+}
--- Web project/home.php
+++ Web project/home.php
@@ -17,15 +17,180 @@
 <body>
 <div class="popup-container" id="popup1" >
     <div class="popup">
-        <div class="close">
-        <i class="fa-sharp fa-regular fa-xmark" onclick="togglepopup()" ></i>
-        </div>
+    <i class="fa-solid fa-rectangle-xmark" id="close" onclick="togglepopup()"></i>
+
         <img src="Img/boeren.jpg" alt="">
-    <h1>Popup</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque, fugit placeat minus accusamus quos, sed dolorum autem blanditiis, 
-        eius velit maiores. Accusamus dolorem ut hic similique quas. Praesentium, impedit amet!</p>
+    <h1>Kleinere bierbrouwers hebben het zwaar door hoge kosten</h1>
+    <p>Jarenlang nam het aantal kleine brouwerijen in Nederland alleen maar toe. 
+        Maar vorig jaar en de eerste helft van dit jaar kwamen er aanzienlijk 
+        minder ondernemingen bij. Ook stopten er vrijwel net zoveel brouwers 
+        als er bij kwamen, blijkt uit cijfers van de stichting Erfgoed
+         Nederlandse Biercultuur.</p>
+         <img src="Img/bierbrouwerijen.jpg" alt="" id="img2">
+         <p>Tot en met 2021 kwamen er jaarlijks zo'n 100 nieuwe brouwers bij. Dit jaar staat de teller op 18, tegenover 14 brouwers die zijn gestopt. 
+            De stagnatie volgt na jaren van forse groei. Nederland telt nu 947 brouwers.</p>
+
+        <h2>Grondstoffen, energie, statiegeld</h2>
+        <p>Jan Ausems van de stichting Erfgoed Nederlandse Biercultuur zegt dat ondernemers steeds meer last hebben van hoge energiekosten en dure grondstoffen (hop, graan, glas en karton). En dan 
+            zijn er de nieuwe regels voor statiegeld die geld kosten. Desondanks, zegt Ausems, 
+            heeft Nederland nog altijd het hoogste aantal brouwers per hoofd van de bevolking.</p>
+            <br>
+            <br>
+            <br>
+            <h2>Bron:Nos.NL</h2>
     </div>
     </div>
+
+
+    <div class="popup-container2" id="popup2" >
+    <div class="popup2">
+    <i class="fa-solid fa-rectangle-xmark" id="close1" onclick="togglepopup2()"></i>
+
+        <img src="Img/jeugdtraining.jpg" alt="">
+    <h1>Jeugdtrainer Koninklijke HFC op non-actief na meldingen grensoverschrijdend gedrag</h1>
+    <p>Een jeugdtrainer van voetbalvereniging Koninklijke HFC uit Haarlem is
+         door het bestuur op non-actief gezet. Er waren meldingen bij de club 
+         binnengekomen van seksueel grensoverschrijdend gedrag online, meldt de vereniging.
+         De trainer mag ook het terrein van HFC niet meer op.
+Volgens de club heeft
+ de trainer via het socialemediaplatform Snapchat om afbeeldingen van
+  geslachtsdelen van jonge leden gevraagd. In maart kwam al een melding van een 
+  andere club waar de trainer actief was binnen bij HFC.
+Niet lang daarna kreeg de club verschillende meldingen van seksueel grensoverschrijdend gedrag van de jeugdtrainer. De club meldt dat er
+ "op dit moment geen aanwijzingen zijn dat dit gedrag ook fysiek heeft plaatsgevonden".</p>
+        <h2>Twee aangiftes</h2>
+        <p>HFC zegt geschokt te zijn door de meldingen. Er zijn zeker vier meldingen binnengekomen, van twee daarvan is aangifte gedaan bij de politie.
+
+De meldingen zijn doorgezet naar het Instituut Sportrechtspraak (ISR), 
+dat een onderzoek is begonnen. HFC meldt verder dat de jeugdtrainer van
+ de KNVB geen trainingen meer mag geven en 
+geen contact meer mag hebben met jonge leden van de vereniging.
+De politie heeft de jeugdtrainer op 7 juni aangehouden.
+ Daarbij is volgens de vereniging "de benodigde digitale
+  informatie zeker gesteld". De trainer is nu op vrije voeten,
+   maar blijft wel verdachte in de zaak, bevestigt de politie 
+   aan RTL Nieuws . De leden zijn vanochtend bijgepraat, meldt RTL.
+ Daarbij waren ook de politie, GGD en Slachtofferhulp aanwezig.</p>
+            <br>
+            <br>
+            <br>
+            <h2>Bron:Nos.NL</h2>
+    </div>
+    </div>
+
+
+
+    <div class="popup-container3" id="popup3" >
+    <div class="popup3">
+    <i class="fa-solid fa-rectangle-xmark" id="close2" onclick="togglepopup3()"></i>
+
+        <img src="Img/zwart-wit.jpg" alt="">
+    <h1>Resten in graf Leipzig inderdaad van Rijksdagbrandstichter Van der Lubbe</h1>
+    <p>De stoffelijke resten in een graf in Leipzig zijn inderdaad van Rijksdagbrandstichter Marinus van der Lubbe, concluderen forensisch pathologen na maanden onderzoek. Tot nu toe was daar nog onzekerheid over. Om duidelijkheid te krijgen, werden de overblijfselen begin dit jaar opgegraven. Onderzoekers vergeleken dna-monsters van
+         een tand met een speekselmonster van een kleinzoon van de broer van Van der Lubbe.
+
+Van der Lubbe was een 24-jarige partijloze communist uit Leiden.
+ Op de avond van 27 februari 1933 stichtte hij brand in het 
+ Rijksdaggebouw in Berlijn, dat na enkele minuten in lichterlaaie stond
+  en binnen twee uur volledig uitbrandde. Over de toedracht van de brand
+   zijn verschillende lezingen: onduidelijk is of Van der Lubbe alleen heeft 
+   gehandeld of hulp kreeg van andere communisten.
+
+Ook zijn er scenario's waarin de nazi's verantwoordelijk worden gesteld voor
+ de brand, die ze aangrepen om in brede kring af te rekenen met communisten.
+  Van der Lubbe beweerde bij zijn arrestatie dat hij de brand had gesticht om
+   de Duitse arbeiders te waarschuwen voor het gevaar van het nationaalsocialisme.
+
+In het najaar van 1933 moest Van der Lubbe zich verantwoorden bij het
+ Rijksgerechtshof in Leipzig. Daarbij maakte hij een apathische indruk.
+  Destijds werd verondersteld dat hij scopolamine toegediend had gekregen, 
+  ook wel "waarheidsserum" of "duivelsadem" genoemd. Bij hoge doses veroorzaakt
+   dat een delier, waarna vermoeidheid, slaperigheid en geheugenverlies optreden.
+
+Daarom zochten toxicologen afgelopen maanden ook naar bewijs voor het verhaal 
+dat Van der Lubbe gedrogeerd was. Dat vonden zij niet. Toch is het volgens 
+hen nog steeds mogelijk dat Van der Lubbe onder invloed was van drugs tijdens het 
+proces. "89 jaar na het overlijden is het erg moeilijk om dergelijk wetenschappelijk
+ bewijs te leveren." Van der Lubbe werd ter dood veroordeeld en kort na het vonnis 
+ onthoofd.</p>
+
+        <h2>Historische mythe ontkracht</h2>
+       <p>De onderzoekers zijn er ook in geslaagd om een 
+        historische mythe te ontkrachten. Van der Lubbe zou 
+        extra diep begraven liggen om te voorkomen dat hij opnieuw wordt opgegraven.
+         Maar dat blijkt niet te kloppen: de resten van de jonge communist werden gevonden
+          op een normale diepte van twee meter.
+
+Van der Lubbe werd in januari opgegraven op initiatief van de Paul Benndorf-Gesellschaft, een stichting die zich bezighoudt met het onderhoud van historische graven. De stichting wilde weten
+ waar Van der Lubbe precies begraven was, om daar een monument te plaatsen.</p>
+            <br>
+            <br>
+            <br>
+            <h2>Bron:Nos.NL</h2>
+    </div>
+    </div>
+
+
+
+    <div class="popup-container4" id="popup4" >
+    <div class="popup4">
+    <i class="fa-solid fa-rectangle-xmark" id="close3" onclick="togglepopup4()"></i>
+
+        <img src="Img/vrouw_snuiten.jpg" alt="">
+    <h1>Ziekmelden om extreme hooikoorts, patiënten snakken naar regen</h1>
+   <p>Iedereen kent wel iemand met hooikoorts. Waar de één de hele buurt bij 
+    elkaar niest, snottert de ander de hele dag door. Sommigen hebben er zoveel 
+    last van dat werken of studeren niet meer gaat.
+
+Een paar weken geleden waarschuwden experts vanwege de 
+combinatie van het natte en warme voorjaar en de droogte van de laatste weken voor 
+een pollenexplosie. Dat heeft basisschoolleraar Lisette ter Beest geweten. 
+"Het is deze weken echt heel heftig. Normaal 
+heb ik al medicatie van de huisarts, maar nu heb ik zelfs een puffer meegekregen 
+tegen de klachten."
+
+Het werd deze week zo erg dat Ter Beest voor het eerst halverwege
+ de dag haar werk moest staken. "Door al dat hoesten was ik mijn stem
+  kwijtgeraakt en moest ik naar huis. Dat heb ik nog nooit gehad. Het zit bij mij echt in
+   de luchtwegen, dus ik moet veel niezen, veel hoesten, en heb jeuk aan mijn ogen,
+    mijn keel en gehemelte."</p>
+         <img src="Img/pollen.jpg" alt="" id="img3">
+        <p>Met milde klachten kun je prima functioneren met medicijnen van de drogist of een extra middeltje van de huisarts. Maar 
+            sommigen hebben er zoveel last van dat ze naar een specialist moeten in 
+            het ziekenhuis. "Het heet niet voor niets hooikoorts: als je er echt ziek
+             van wordt, kan het voelen alsof je de griep hebt", zegt kinderarts en 
+             allergoloog in het Deventer Ziekenhuis Monique Gorissen in het radioprogramma
+              Nieuws en Co.
+
+Hoewel hooikoorts zich normaliter pas op latere leeftijd ontwikkelt, ziet de arts dat de 
+klachten zich op steeds jongere leeftijd voordoen. Ook wordt de groep met extreme
+ klachten volgens haar steeds groter.</p>
+ <br>
+ <br>
+ <p>Volgens allergoloog Gorissen zijn we voorlopig nog niet af van die heftige hooikoortsseizoenen. "We weten het natuurlijk nooit precies, maar de verwachting is dat hier in de zomer minder neerslag valt en dat het warmer is. Die twee dingen
+     samen maken dat het flink stuift", aldus de arts. Slecht nieuws voor 
+     hooikoortspatiënten dus.
+
+Maar voor de korte termijn is er ook goed nieuws: de regendans van Vincent
+ heeft geholpen. Vanaf morgen valt er her en der een lokale bui en ook de
+  komende dagen zal er af en toe wat regen vallen. Die regen zorgt voor schone
+   lucht en vermindert het aantal pollen: een beetje verlichting dus voor al die 
+   hooikoortspatiënten in Nederland.
+
+Ter Beest kan in ieder geval niet wachten tot de eerste druppels vallen: "Het is zo'n
+ cliché, want als het heet is, klagen we over de hitte. Is het koud, dan klagen we dat 
+ het warmer moet zijn. Maar een klein beetje regen zou nu toch wel lekker zijn. Niet 
+ alleen voor mij, maar ook voor de natuur."</p>
+            <br>
+            <br>
+            <br>
+            <h2>Bron:Nos.NL</h2>
+    </div>
+    </div>
+
+
+
+
     <header>
         <nav>
             <a href="home.php"><img src="Img/GroenLinks_logo.png" alt=""></a>
@@ -70,22 +235,19 @@
             <div class="card">
                 <h2>Jeugdtrainer Koninklijke HFC op non-actief na meldingen grensoverschrijdend gedrag</h2>
                 <img src="Img/jeugdtraining.jpg" alt="">
-                <button>Lees meer</button>
+                <button onclick="togglepopup2()">Lees meer</button>
             </div>
 
             <div class="card">
                 <h2>Resten in graf Leipzig inderdaad van Rijksdagbrandstichter Van der Lubbe</h2>
                 <img src="Img/zwart-wit.jpg" alt="">
-                <button>Lees meer</button>
+                <button onclick="togglepopup3()">Lees meer</button>
             </div>
             <div class="card">
                 <h2>Ziekmelden om extreme hooikoorts, patiënten snakken naar regen</h2>
                 <img src="Img/vrouw_snuiten.jpg" alt="">
-                <button>Lees meer</button>
+                <button onclick="togglepopup4()">Lees meer</button>
             </div>
-        </div>
-        <div class="LEEES">
-            <button class="Lees">LEES MEER</button>
         </div>
         <br>
         <br>
